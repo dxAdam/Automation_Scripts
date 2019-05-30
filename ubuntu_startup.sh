8888c4d35f3e99fe407b0d2b498840f843509541
dxadam <adamgrusky@msn.com>
2019-05-30 11:44:34 -0400

    added option to install graphic drivers

---
+++ ubuntu_startup.sh
@@ -0,0 +1,17 @@
+#apt update &&
+#apt upgrade &&
+
+#apt install vim
+#apt install gnome-tweaks
+
+
+
+read -p "Install nvidia drivers? (y/n)?" choice
+case "$choice" in 
+  y|Y ) add-apt-repository ppa:graphics-drivers/ppa &&
+	apt install libnvidia-cfg1-430 &&
+	ubuntu-drivers autoinstall &&
+        apt autoremove;;
+  * ) echo "skipping nvidia drivers";;
+esac
+

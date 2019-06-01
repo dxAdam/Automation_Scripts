567ae4d06520727584afb15f8636071bc81971da
dxadam <adamgrusky@msn.com>
2019-06-01 14:03:41 -0400

    added comment at top

--- after_reboot.sh
+++ after_reboot.sh
@@ -1,3 +1,5 @@
+# run this after rebooting once ubuntu_startup.sh has run
+
 # enable overclocking / fan control of GPU ... additional reboot required to take effect
 nvidia-xconfig -a --cool-bits=28 --allow-empty-initial-configuration
 

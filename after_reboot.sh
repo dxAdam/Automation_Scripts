4df0ab228a6a4fd5ba1a04c55f8ad4bffeab2bcc
dxadam <adamgrusky@msn.com>
2019-09-20 15:05:58 -0400

    added notes at top of file

--- after_reboot.sh
+++ after_reboot.sh
@@ -1,4 +1,8 @@
 # run this after rebooting once ubuntu_startup.sh has run
+#
+# you may need to run "sudo chmod +x after_reboot.sh" to make executable 
+#
+# note: if running the Nvidia GPU overclocking program "Green With Envy" (GWE) and most features are grayed out it means the coolbits didn't set. It's possible the drivers overwrote the X11 config once fully configured/installed from additonal updates. Try running this script again and checking GWE after reboot.
 
 # enable overclocking / fan control of GPU ... additional reboot required to take effect
 flatpak update &&

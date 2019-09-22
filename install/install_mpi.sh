4fd31da47098d8b8ae9c9125460ee5bbdd07562e
dxadam <adamgrusky@msn.com>
2019-09-21 23:32:41 -0400

    fixed source .bashrc error

--- install/install_mpi.sh
+++ install/install_mpi.sh
@@ -1,4 +1,7 @@
+
+#set desired version
 VERSION=4.0.1
+
 
 wget https://download.open-mpi.org/release/open-mpi/v4.0/openmpi-$VERSION.tar.gz &&
 tar -xf openmpi* &&
@@ -12,7 +15,7 @@ sudo make install
 
 echo export PATH="$PATH:/home/$USER/.openmpi/bin" >> /home/$USER/.bashrc &&
 echo export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:/home/$USER/.openmpi/lib/">> /home/$USER/.bashrc &&
-source /home/$USER/.bashrc
+
 
 
 if test -f "openmpi-$VERSION.tar.gz"; then
@@ -20,8 +23,6 @@ if test -f "openmpi-$VERSION.tar.gz"; then
 fi
 
 
-echo -e 'testing ...\n\n'
-mpirun --version &&
-echo -e '' &&
-mpicc --showme:version &&
-echo -e '\n\n... openmpi successfully installed'
+# source ~/.bashrc will not work here since this shell script is
+# running in its own shell instance. exec bash will work however.
+exec bash

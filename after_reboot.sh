51a1ddb9cb903dbc7caaf22942b30bda3b2c635b
dxadam <adamgrusky@msn.com>
2019-10-30 20:31:06 -0400

    added shebang

--- after_reboot.sh
+++ after_reboot.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+#
 # run this after rebooting once ubuntu_startup.sh has run
 #
 # you may need to run "sudo chmod +x after_reboot.sh" to make executable 
@@ -6,6 +8,17 @@
 # are grayed out it means the coolbits didn't set. It's possible the drivers overwrote the X11
 # config once fully configured/installed from additonal updates. Try running this script again
 # and checking GWE after reboot.
+
+
+#
+# Check script was ran as root
+#
+
+if [[ $EUID -ne 0 ]]; then
+   echo -e "This script must be run as root!\nhint: use 'sudo !!' to run a previously entered command with root priveleges" 
+   exit 1
+fi
+
 
 # enable overclocking / fan control of GPU ... additional reboot required to take effect
 flatpak update &&

45a71a07da36a66ab1aa7dd2fe8fb284ee2f5be7
dxadam <adamgrusky@msn.com>
2020-03-05 20:50:49 -0500

    fixed typo

--- BeagleBone/init_beag.sh
+++ BeagleBone/init_beag.sh
@@ -12,11 +12,11 @@ cd /sys/class/net && select beagle in *; do break; done
 echo $beagle selected
 
 
+sudo /sbin/sysctl -w net.ipv6.conf.all.forwarding=1
+sudo /sbin/sysctl -w net.ipv4.ip_forward=1
 
 sudo iptables --table nat --append POSTROUTING --out-interface $internet -j MASQUERADE
 sudo iptables --append FORWARD --in-interface $beagle -j ACCEPT
 
-sudo /sbin/sysctl -w net.ipv4.ip_forward=1
-sudo /sbin/sysctl -w net.ipv6.conf.all.forwarding=1
 
 ssh debian@192.168.7.2

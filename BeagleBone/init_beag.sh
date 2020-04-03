5fd74fcc71a472b4a0c5fa2388e9eccf715c3d56
dxadam <adamgrusky@msn.com>
2020-04-03 15:31:03 -0400

    uploaded init_beag.sh

---
+++ BeagleBone/init_beag.sh
@@ -0,0 +1,22 @@
+
+ifconfig
+
+# prompt user to select Internet interface
+echo "Select your Internet interface:"
+cd /sys/class/net && select internet in *; do break; done
+echo $internet selected
+
+# promp user to select BeagleBone interfac
+echo -e "\nSelect your BeagleBone interface:"
+cd /sys/class/net && select beagle in *; do break; done
+echo $beagle selected
+
+# set up forwarding in iptables
+sudo iptables --table nat --append POSTROUTING --out-interface $internet -j MASQUERADE
+sudo iptables --append FORWARD --in-interface $beagle -j ACCEPT
+
+# enable forwarding
+sudo sh -c "echo 1 > /proc/sys/net/ipv4/ip_forward"
+
+# connect to Beagle
+ssh debian@192.168.7.2

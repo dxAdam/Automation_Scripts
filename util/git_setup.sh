dfe2a9c78fe3284a86766370511bead439eaaebc
dxadam <adamgrusky@msn.com>
2020-04-12 22:00:29 -0400

    changed way username and email are obtained

--- util/git_setup.sh
+++ util/git_setup.sh
@@ -4,13 +4,12 @@
 # This script configures git with user.name, user.email, and the credential storer - which allows you to push without entering your username and password after doing it at least once during the session.
 #
 
+echo "Enter git username"
+read GITUSER
+#git config --global user.name $GITUSER
 
-if [ "$#" -ne 2 ]; then
-    echo "This script needs your git user.name and user.email as arguments!"
-fi
-
-git config --global user.name $1
-
-git config --global user.email $2
+echo "Enter git email"
+read GITEMAIL
+git config --global user.email $GITEMAIL
 
 git config --global credential.helper store

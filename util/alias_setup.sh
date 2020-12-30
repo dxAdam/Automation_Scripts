44ef3f7f9f7d285bf40ad939a845f558ef3a76b4
Demo User <adamgrusky@msn.com>
2020-12-30 03:43:52 +0000

    added dpkg-sort.sh to alias_setup.sh

--- util/alias_setup.sh
+++ util/alias_setup.sh
@@ -13,11 +13,11 @@ cp resetsound.sh ~/.alias_scripts
 cp git_setup.sh ~/.alias_scripts
 cp dpkg-sort.sh ~/.alias_scripts
 
-echo "alias check_raid='/home/$USER/.alias_scripts/check_raid.sh'" >> /home/$USER/.bashrc
+echo "alias check-raid='/home/$USER/.alias_scripts/check_raid.sh'" >> /home/$USER/.bashrc
 echo "alias cpubench='/home/$USER/.alias_scripts/cpubench.sh'" >> /home/$USER/.bashrc
-echo "alias gwe='/home/$USER/.alias_scripts/gwe.sh'" >> /home/$USER/.bashrc
-echo "alias git setup='/home/$USER/.alias_scripts/git_setup.sh'" >> /home/$USER/.bashrc
-echo "alias reset sound='/home/$USER/.alias_scripts/resetsound.sh'" >> /home/$USER/.bashrc
+#echo "alias gwe='/home/$USER/.alias_scripts/gwe.sh'" >> /home/$USER/.bashrc
+echo "alias git-setup='/home/$USER/.alias_scripts/git_setup.sh'" >> /home/$USER/.bashrc
+echo "alias reset-sound='/home/$USER/.alias_scripts/resetsound.sh'" >> /home/$USER/.bashrc
 echo "alias dpkg-sort='/home/$USER/.alias_scripts/dpkg-sort.sh'" >> /home/$USER/.bashrc
 
 echo "alias python='python3'" >> /home/$USER/.bashrc

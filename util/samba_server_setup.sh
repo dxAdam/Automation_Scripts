a511e4c20771e2b7230539a80c4cf91a7c3af1fc
Demo User <adamgrusky@msn.com>
2020-12-30 03:43:09 +0000

    improved samba server set up

--- util/samba_server_setup.sh
+++ util/samba_server_setup.sh
@@ -3,11 +3,16 @@
 sudo apt install samba samba-common-bin
 
 sudo mkdir /samba
-sudo chmod -R 777 /samba
 
 echo -e '\nEnter Samba username: '
 read username
 
+sudo adduser $username
+
+sudo smbpasswd -a $username
+
 sudo bash -c "echo -e '[smb-shared]\npath=/samba\nbrowsable=yes\nwritable=yes\ncreate mask=0644\ndirectory mak=0755\nforce user=$username' >> /etc/samba/smb.conf"
+
+sudo chown -R dxadam:dxadam /samba
 
 sudo systemctl restart smbd.service

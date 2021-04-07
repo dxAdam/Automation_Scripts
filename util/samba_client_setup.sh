fe092f71953c3c3bba122e657faef987b34e5947
dxadam <adamgrusky@msn.com>
2021-04-07 19:47:37 -0400

    updated samba config

--- util/samba_client_setup.sh
+++ util/samba_client_setup.sh
@@ -12,11 +12,16 @@ read username
 echo -e '\nEnter Samba password: '
 read -s password
 
+echo -e '\nEnter shared drive name: '
+read drivename
+
 sudo bash -c "echo 'username=$username' >> /root/.smbcredentials"
 sudo bash -c "echo 'password=$password' >> /root/.smbcredentials"
 
+sudo mkdir /media/$drivename
+
 # mount shared disk
-sudo mount.cifs -v //192.168.1.102/smb-shared /media/samba --verbose -o user=$username,password=$password,domain=WORKGROUP
+sudo mount.cifs -v //$smbip/$drivename /media/$drivename --verbose -o user=$username,password=$password,domain=WORKGROUP
 
 # auto-mount on boot by adding entry to /etc/fstab
-sudo bash -c "echo '//$smbip/smb-shared /media/samba cifs uid=$username,credentials=/root/.smbcredentials,vers=3.0' >> /etc/fstab"
+sudo bash -c "echo '//$smbip/$drivename /media/$drivename cifs uid=$username,credentials=/root/.smbcredentials,vers=3.0' >> /etc/fstab"
